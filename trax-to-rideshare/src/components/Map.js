2f978e3d78da94c6f313536fded396f421675e4c
Thien Nguyen <nguthien2002@gmail.com>
2022-07-27 01:17:52 -0600

    Added options for the Map ID, added markers of an 2D array

--- trax-to-rideshare/src/components/Map.js
+++ trax-to-rideshare/src/components/Map.js
@@ -1,17 +1,39 @@
 import { useMemo } from "react";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 
+var currentPrice = 0;
+//Name, lat, lng, scaledWidth, scaledHeight, price
+const markers = [
+  [
+    "Old Bingham Hwy",
+    40.58656608198297,
+    -111.9950785441254,
+    currentPrice.toString(),
+  ],
+];
+
 function Map() {
   const center = useMemo(() => ({ lat: 40.7608, lng: -111.891 }), []);
 
   return (
     <GoogleMap
-      zoom={12}
+      zoom={11.5}
       center={center}
       mapContainerClassName="map-container"
-      options={{ mapId: "bcdb35cb304e64db" }}
+      options={{
+        mapId: "bcdb35cb304e64db",
+        mapTypeControl: false,
+        fullscreenControl: false,
+        streetViewControl: false,
+      }}
     >
-      <Marker position={center} />{" "}
+      {markers.map((marker) => (
+        <Marker
+          key={marker[0]}
+          position={{ lat: marker[1], lng: marker[2] }}
+          title={marker[0] + " " + marker[3]}
+        />
+      ))}
     </GoogleMap>
   );
 }
