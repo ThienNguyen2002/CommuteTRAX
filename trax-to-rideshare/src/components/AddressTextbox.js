9f9cadd17c14d291c6a4a1bc812518d69d35a6a3
Thien Nguyen <nguthien2002@gmail.com>
2022-08-02 21:20:28 -0600

    Deleted

--- trax-to-rideshare/src/components/AddressTextbox.js
+++
@@ -1,9 +0,0 @@
-const AddressTextBox = () => {
-  return (
-    <div className="address-textbox">
-      <input type="text" placeholder="Search for an address" />
-    </div>
-  );
-};
-
-export default AddressTextBox;
