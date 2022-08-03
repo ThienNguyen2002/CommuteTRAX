335bb1d17d7b45526020fc2e139d79032eb35669
Thien Nguyen <nguthien2002@gmail.com>
2022-08-02 21:22:22 -0600

    Added autocomplete, deleted the wrapper

--- trax-to-rideshare/src/App.js
+++ trax-to-rideshare/src/App.js
@@ -1,37 +1,39 @@
-import React from "react";
-import AddressTextBox from "./components/AddressTextbox";
+import React, { useState } from "react";
+//import AddressTextBox from "./components/AddressTextbox";
 import Map from "./components/Map";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import BarLoader from "react-spinners/BarLoader";
-import { ErrorBoundary } from "react-error-boundary";
 
-const render = (status) => {
-  // eslint-disable-next-line default-case
-  switch (status) {
-    case Status.LOADING:
-      return <BarLoader />;
-    case Status.FAILURE:
-      return (
-        <ErrorBoundary>
-          {" "}
-          <Map />{" "}
-        </ErrorBoundary>
-      );
-    case Status.SUCCESS:
-      return <Map />;
-  }
-};
+import { useLoadScript, Marker } from "@react-google-maps/api";
+import PlacesAutocomplete from "./components/PlacesAutocomplete";
 
 function App() {
+  const [libraries] = useState(["places"]);
+  const { isLoaded } = useLoadScript({
+    //googleMapsApiKey: process.env.GM_API_KEY,
+    googleMapsApiKey: process.env.GM_API_KEY,
+    libraries: libraries,
+  });
+
+  const [selected, setSelected] = useState(null);
+
+  const searchOptions = {
+    types: ["(address)"],
+    componentRestrictions: { country: "us" },
+  };
+
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <div className="App">
       {" "}
       <div className="AddressContainer">
-        <AddressTextBox />
+        <PlacesAutocomplete
+          setSelected={setSelected}
+          searchOptions={searchOptions}
+        />
       </div>
-      <Wrapper apiKey={process.env.GM_API_KEY} render={render}>
-        <Map />
-      </Wrapper>
+      <div className="MapContainer">
+        <Map>{selected && <Marker position={selected} />}</Map>
+      </div>
     </div>
   );
 }
